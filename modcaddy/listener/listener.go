46b5ca3c80a744e9a197c901c069d62d0c42e181
Jackson Sippe <33528902+sippejw@users.noreply.github.com>
2026-05-27 14:27:42 -0600

    Update listener.go
    Signed-off-by: Jackson Sippe <33528902+sippejw@users.noreply.github.com>

--- modcaddy/listener/listener.go
+++ modcaddy/listener/listener.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
-	"github.com/gaukas/clienthellod/modcaddy/app"
+	"github.com/sippejw/clienthellod/modcaddy/app"
 	"go.uber.org/zap"
 )
 
